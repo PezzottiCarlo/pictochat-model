27da315ae24b978d0ac0970a0c6def242c948584
Carlo Pezzotti <carlo.pezzotti@student.supsi.ch>
2024-11-08 18:27:44 +0100

    Update app.run() to bind to all network interfaces

--- api.py
+++ api.py
@@ -19,5 +19,5 @@ def lemmatize_word():
     return jsonify({"original": word, "lemmatized": lemmatized_word})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=10000)
     
