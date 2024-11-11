3127a467e1c7efb782d428e62dc7d59af35fa94c
Carlo Pezzotti <carlo.pezzotti@student.supsi.ch>
2024-11-11 17:27:07 +0100

    Refactor lemmatize_word() function in api.py

--- api.py
+++ api.py
@@ -10,13 +10,28 @@ print(nlp._path)
 @app.route('/lemmatize', methods=['POST'])
 def lemmatize_word():
     data = request.get_json()
+    if data is None:
+        return jsonify({"lemmatized_words": []})
+    
+    if "word" not in data:
+        return jsonify({"lemmatized_words": []})
+    
     word = data.get("word", "")
-
-    if not word:
-        return jsonify({"error": "No word provided"}), 400
-    doc = nlp(word)
-    lemmatized_word = doc[0].lemma_
-    return jsonify({"original": word, "lemmatized": lemmatized_word})
+    
+    if word == "":
+        return jsonify({"lemmatized_words": []})
+    
+    words = word.split(" ")
+    lemmatized_words = []
+    
+    if len(words) == 0:
+        return jsonify({"lemmatized_words": lemmatized_words})
+    
+    for word in words:
+        doc = nlp(word)
+        for token in doc:
+            lemmatized_words.append(token.lemma_)
+    return jsonify({"lemmatized_words": lemmatized_words})
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=10000)
